d089cfadd4af10841326931be32c06ba5d2c2c2e
Erathaowl <46474708+erathaowl@users.noreply.github.com>
2023-04-10 09:04:27 +0200

    Update setup.py
    Updated package info

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as f:
 
 setup(
     name='nanoid',
-    version='2.0.0',
-    author='Paul Yuan',
-    author_email='puyuan1@gmail.com',
+    version='3.0.0',
+    author='Erathaowl',
+    author_email='erathaowl@gmail.com',
     description='A tiny, secure, URL-friendly, unique string ID generator for Python',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/puyuan/py-nanoid',
+    url='https://github.com/erathaowl/py-nanoid',
     license='MIT',
     packages=['nanoid'],
     classifiers=[
