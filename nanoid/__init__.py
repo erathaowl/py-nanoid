e59e3d07bfdf312032b1c2cb8626e96e4b8c0f95
Erathaowl <46474708+erathaowl@users.noreply.github.com>
2023-04-10 08:54:36 +0200

    Update __init__.py
    Code refactored in more simple and pythonic way (https://github.com/puyuan/py-nanoid/issues/13)

--- nanoid/__init__.py
+++ nanoid/__init__.py
@@ -1,6 +1,25 @@
 # coding: utf-8
 
-from nanoid.generate import generate
-from nanoid.non_secure_generate import non_secure_generate
+from functools import reduce
+from operator import concat
 
-__all__ = ['generate', 'non_secure_generate']
+import random
+import secrets
+Import string
+
+alphabet_std = f"_-{string.digits}{string.ascii_letters}"
+# alphabet_std = "_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alphabet_hum = "_-23456789abcdefghijkmnpqrstuvwxyzABCDEFGHIJKMNPQRSTUVWXYZ"
+
+default_size = 21
+
+
+def non_secure_generate(alphabet=alphabet_std, size=default_size):    
+    return reduce(concat, (random.choice(alphabet) for _ in range(size)))
+
+
+def generate(alphabet=alphabet_std, size=default_size):
+    return reduce(concat, (secrets.choice(alphabet) for _ in range(size)))
+
+
+__all__ = ["generate", "non_secure_generate", "alphabet_std", "alphabet_hum"]
